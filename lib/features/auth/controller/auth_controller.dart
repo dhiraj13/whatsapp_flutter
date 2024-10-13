3b635c9488f253185bdda20a423c7ed1c64487a9
Dhiru <dkhapangi13@outlook.com>
2024-10-13 10:41:00 +0545

    Persist Auth State

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -3,10 +3,16 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_flutter/features/auth/repository/auth_repository.dart';
+import 'package:whatsapp_flutter/models/user_model.dart';
 
 final authControllerProvider = Provider((ref) {
   final authRepository = ref.watch(authRepositoryProvider);
   return AuthController(authRepository: authRepository, ref: ref);
+});
+
+final userDataAuthProvider = FutureProvider((ref) {
+  final authController = ref.watch(authControllerProvider);
+  return authController.getUserData();
 });
 
 class AuthController {
@@ -18,11 +24,20 @@ class AuthController {
     required this.ref,
   });
 
+  Future<UserModel?> getUserData() async {
+    UserModel? user = await authRepository.getCurrentUserData();
+    return user;
+  }
+
   void signInWithPhone(BuildContext context, String phoneNumber) {
     authRepository.signInWithPhone(context, phoneNumber);
   }
 
-  void verifyOTP(BuildContext context, String verificationId, String userOTP) {
+  void verifyOTP(
+    BuildContext context,
+    String verificationId,
+    String userOTP,
+  ) {
     authRepository.verifyOTP(
       context: context,
       verificationId: verificationId,
@@ -31,7 +46,10 @@ class AuthController {
   }
 
   void saveUserDataToFirebase(
-      BuildContext context, String name, File? profilePic) {
+    BuildContext context,
+    String name,
+    File? profilePic,
+  ) {
     authRepository.saveUserDataToFirebase(
       name: name,
       profilePic: profilePic,
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_flutter/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:whatsapp_flutter/common/widgets/Loader.dart';
+import 'package:whatsapp_flutter/common/widgets/error.dart';
+import 'package:whatsapp_flutter/features/auth/controller/auth_controller.dart';
 import 'package:whatsapp_flutter/features/landing/screens/landing_screen.dart';
 import 'package:whatsapp_flutter/firebase_options.dart';
 import 'package:whatsapp_flutter/router.dart';
+import 'package:whatsapp_flutter/screens/mobile_layout_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,11 +22,11 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Whatsapp Flutter',
@@ -33,7 +37,20 @@ class MyApp extends StatelessWidget {
         ),
       ),
       onGenerateRoute: (settings) => generateRoute(settings),
-      home: const LandingScreen(),
+      home: ref.watch(userDataAuthProvider).when(
+            data: (user) {
+              if (user == null) {
+                return const LandingScreen();
+              }
+              return const MobileLayoutScreen();
+            },
+            error: (err, trace) {
+              return ErrorScreen(
+                error: err.toString(),
+              );
+            },
+            loading: () => const Loader(),
+          ),
     );
   }
 }
